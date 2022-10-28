c0121e7588fedbb4d8f5fe9073195a12d23566c1
LEAP\Gabriel.McHugh <gabriel.t.mchugh@gmail.com>
2022-10-28 11:22:00 +1000

    added ngSwitch

--- src/app/components/directives.component.ts
+++ src/app/components/directives.component.ts
@@ -1,11 +1,17 @@
-import { Component } from "@angular/core";
+import { Component, Input } from "@angular/core";
 
 @Component({
     selector: 'directives',
-    templateUrl: './directives.component.html'
+    templateUrl: './directives.component.html',
+    styles: [`
+        .card {
+            margin-bottom: 1rem
+        }
+    `]
 })
 export class DirectivesComponent {
     courses: boolean = true;
+    viewMode: 'map' | 'list' = 'map'
 
     onClick() {
         this.courses = !this.courses
