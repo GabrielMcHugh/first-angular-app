95e8642ea65b7dbf0b487c29e61a74c175637d5e
LEAP\Gabriel.McHugh <gabriel.t.mchugh@gmail.com>
2022-10-28 10:52:17 +1000

    arrange layout

--- src/app/components/directives.component.ts
+++ src/app/components/directives.component.ts
@@ -2,16 +2,7 @@ import { Component } from "@angular/core";
 
 @Component({
     selector: 'directives',
-    template: `
-        <h2 (click)="onClick()">Directives</h2>
-        <div *ngIf="courses; then coursesList else noCourses"></div>
-        <ng-template #coursesList>
-            List of courses
-        </ng-template>
-        <ng-template #noCourses>
-            No courses yet
-        </ng-template>
-    `
+    templateUrl: './directives.component.html'
 })
 export class DirectivesComponent {
     courses: boolean = true;
